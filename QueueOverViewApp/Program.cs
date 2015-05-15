a7f7f9fd204c0ac17e873b2110c307b5aad06f57
arrobin <ashiqur007@yahoo.com>
2015-05-15 22:48:05 +0600

    Understanding Queue

---
+++ QueueOverViewApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueOverViewApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> myQueue = new Queue<string>();
+
+            myQueue.Enqueue("Hellow");
+            myQueue.Enqueue("Robin");
+
+            Console.WriteLine(myQueue.Dequeue());
+            Console.WriteLine(myQueue.Peek());
+            Console.ReadKey();
+        }
+    }
+}
